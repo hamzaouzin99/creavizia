5d154b4b7ba679f74746fbc5392d4c21390a8814
hamza <hamzaouzin21@gmail.com>
2023-11-30 12:30:57 +0100

    Add Script

--- app/layout.js
+++ app/layout.js
@@ -24,8 +24,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ar">
     <Head>
-      <Script>
-        {
+      <script>
+        {`
           
          
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -33,10 +33,10 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-K46QRVX8')
-
+`
           
         }
-      </Script>
+      </script>
     </Head>
     <body  className='overflow-x-hidden'>
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K46QRVX8"
